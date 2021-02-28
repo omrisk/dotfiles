f80bf6b061f058bbeba9cee2c47106dacd49c99c
omrisk <omrisk@gmail.com>
2021-02-28 11:46:48 +0200

    Adding option to backup installed Brew formulue

--- brew.sh
+++ brew.sh
@@ -6,28 +6,73 @@
 export HOMEBREW_BREWFILE="~/.brewfile"
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
-# Make sure we’re using the latest Homebrew.
-brew update
-
-# Upgrade any already-installed formulae.
-brew upgrade
-
 # Save Homebrew’s installed location.
 BREW_PREFIX=$(brew --prefix)
 
-brew bundle --global
 
-# Install GNU core utilities (those that come with macOS are outdated).
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+commit_brewfile(){
+  # Ensure we don't commit stuff we don't want
+  git stash
 
-ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
+  # Backup and save the updated BREW packages
+  brew bundle dump --file=./.Brewfile
+
+  git commit --include .Brewfile -m "Updating brew packages"
+
+  # Push the changes
+  gh pr create --title "Brewfile update" -b "This was opened by using the 'update-brew.sh' script."
+
+  # Restore the localbranch to the pre-update state
+  git stash pop
+}
+
+brew_update(){
+  # Make sure we’re using the latest Homebrew.
+  brew update
+
+  # Upgrade any already-installed formulae.
+  brew upgrade
+
+  brew bundle --global
+
+  # Install GNU core utilities (those that come with macOS are outdated).
+  # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+
+  ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
 
 
-# Switch to using brew-installed bash as default shell
-if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
-  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
-  chsh -s "${BREW_PREFIX}/bin/bash";
-fi;
+  # Switch to using brew-installed bash as default shell
+  if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+    echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
+    chsh -s "${BREW_PREFIX}/bin/bash";
+  fi;
 
-# Remove outdated versions from the cellar.
-brew cleanup
+  # Remove outdated versions from the cellar.
+  brew cleanup
+}
+
+
+while getopts ":buh" opt; do
+  case $opt in
+    b)
+      echo "Backing up brew accoriding to output from 'brew bundle dump'"
+      commit_brewfile
+      exit 0   
+      ;;
+    u)
+      echo "Installing and updating brew formulae"
+      brew_update
+      exit 0   
+      ;;
+    h)
+      echo "Help blurb!"
+      echo "-b - Create a .Brewfile based off the locally installed brew formulae and open a github pull request to commit it"
+      echo "-u - Update brew and install any packages in the global .Brewfile"
+      exit 0
+      ;;
+    *)
+      echo "Default running the update/upgrade (like you passed '-u' to this script)"
+      exit 0
+      ;;
+  esac
+done
