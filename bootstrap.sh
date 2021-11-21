3b255afca875d2ca5cc7f73f6264f57446b4be6f
omrisk <23058747+omrisk@users.noreply.github.com>
2021-11-21 09:58:43 +0200

    fix(brew): install brew if not availble

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,40 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")";
+cd "$(dirname "${BASH_SOURCE}")"
 
-function doIt() {
+function do_it() {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
-		-avh --no-perms . ~;
-	source ~/.bash_profile;
+		-avh --no-perms . ~
+	source ~/.bash_profile
+}
+
+function check_and_install_homebrew() {
+	which -s brew
+	if [[ $? != 0 ]]; then
+		# Install Homebrew
+		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	fi
+
+	# Load functions from the brew script
+	source ./brew.sh --source-only
+	brew_update
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+	do_it
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+	echo ""
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
-unset doIt;
+		do_it
+	fi
+fi
+
+check_and_install_homebrew
+
+unset do_it
+unset check_and_install_homebrew
--- brew.sh
+++ brew.sh
@@ -9,7 +9,7 @@ export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 # Save Homebrew’s installed location.
 BREW_PREFIX=$(brew --prefix)
 
-commit_brewfile() {
+function commit_brewfile() {
   # Ensure we don't commit stuff we don't want
   git stash
 
@@ -18,7 +18,7 @@ commit_brewfile() {
   # Backup and save the updated BREW packages, overwrite the repositories '.Brewfile'
   brew bundle dump --file=./.Brewfile --force
 
-  git commit --include .Brewfile -m "feat(brew): Updating brew packages"
+  git commit --include .Brewfile -m "feat(brew): Updating brew packages for $DATE"
 
   # Push the changes
   gh pr create --title "Brewfile update" -b "This was opened by using the 'update-brew.sh' script."
@@ -28,7 +28,7 @@ commit_brewfile() {
   git stash pop
 }
 
-brew_update() {
+function brew_update() {
   # Make sure we’re using the latest Homebrew.
   brew update
 
@@ -52,38 +52,45 @@ brew_update() {
   brew cleanup
 }
 
-print_help() {
+function print_help() {
   echo "Help blurb!"
   echo "-b - Create a .Brewfile based off the locally installed brew formulae and open a github pull request to commit it"
   echo "-u - Update brew and install any packages in the global .Brewfile"
   echo "-h - Print this help blurb"
 }
 
-if [ "$#" -eq 0 ]; then
+function main() {
+  if [ "$#" -eq 0 ]; then
     echo "No parameters passed, see this helpful help blurb:"
-    brew_update
-fi
+    print_help
+  fi
 
-while getopts ":buh" opt; do
-  case $opt in
-  b)
-    echo "Backing up brew accoriding to output from 'brew bundle dump'"
-    commit_brewfile
-    exit 0
-    ;;
-  u)
-    echo "Installing and updating brew formulae"
-    brew_update
-    exit 0
-    ;;
-  h)
-    print_help
-    exit 0
-    ;;
-  *)
-    echo "invalid flag!"
-    print_help
-    exit 0
-    ;;
-  esac
-done
+  while getopts ":buh" opt; do
+    case $opt in
+    b)
+      echo "Backing up brew accoriding to output from 'brew bundle dump'"
+      commit_brewfile
+      exit 0
+      ;;
+    u)
+      echo "Installing and updating brew formulae"
+      brew_update
+      exit 0
+      ;;
+    h)
+      print_help
+      exit 0
+      ;;
+    *)
+      echo "invalid flag!"
+      print_help
+      exit 0
+      ;;
+    esac
+  done
+
+}
+
+if [[ "${1}" != "--source-only" ]]; then
+  main "${@}"
+fi
